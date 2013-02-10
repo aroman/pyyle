e5b109373f0beeb167382450decd138df914e860
Avi Romanoff <aviromanoff@gmail.com>
2013-02-09 21:34:26 -0500

    Fix host binding and make buttons less-y

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ if __name__ == '__main__':
     app.debug = True
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    if app.debug: host = '127.0.0.1'
-    else: host = '0.0.0.0'
+    if ('PORT' in os.environ): host = '0.0.0.0'
+    else: host = '127.0.0.1'
     app.run(host=host, port=port)

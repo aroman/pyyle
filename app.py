7ee8ec876818b596e9d044b6fae3de9160e377a5
Avi Romanoff <aviromanoff@gmail.com>
2013-02-09 20:31:03 -0500

    Merge in static project

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    app.logger.warning("OH SHIT!")
-    return "Pyyle here."
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.debug = True
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    if app.debug: host = '127.0.0.1'
+    else: host = '0.0.0.0'
+    app.run(host=host, port=port)
